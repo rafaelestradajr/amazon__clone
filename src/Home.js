998809ba818f5843bd3ba392e73c304b8c7867c1
Rafael Estrada <rafaelestrada@comcast.net>
2020-11-01 23:25:34 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className='home'>
+            <p>i am home component</p>
+        </div>
+            
+        
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCzQIYm8srfAAzJBsWZoLyYKkA3MJl9qSM",
+    authDomain: "clone-67f64.firebaseapp.com",
+    databaseURL: "https://clone-67f64.firebaseio.com",
+    projectId: "clone-67f64",
+    storageBucket: "clone-67f64.appspot.com",
+    messagingSenderId: "836647483628",
+    appId: "1:836647483628:web:911f8637a407226b630dc9",
+    measurementId: "G-1LJJSXYX8W"
+  };
