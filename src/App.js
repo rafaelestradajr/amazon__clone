b83af4a946b7737081d2c61474cf89a7c83714bc
Rafael Estrada <rafaelestrada@comcast.net>
2020-11-05 23:14:36 -0800

    bui.lding pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import Header from './Header';
-import './App.css';
-import React from 'react';
-import Home from './Home';
+import Header from "./Header";
+import "./App.css";
+import React from "react";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
     //BEM
-    <div className="app">
-    
-  <Header/>
-  <Home/>
-    </div>
+    <Router>
+      <div className="app">
+            <Header />
+        <Switch>
+
+        <Route path="/checkout">
+          <Checkout/>
+          </Route>
+
+
+
+         <Route path="/">
+           
+            <Home />
+          </Route>
+        </Switch> 
+      </div>
+    </Router>
   );
 }
 
