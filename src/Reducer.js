9203b90a2cc9c78dab6ae84208f4be585b6e84ac
Rafael Estrada <rafaelestrada@comcast.net>
2020-11-15 22:28:56 -0800

    connected firebase

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,5 +1,6 @@
 export const initialState={
     basket:[],
+    user:null
 };
 
 
@@ -35,8 +36,13 @@ const reducer=(state,action)=>{
             return{
                 ...state,
                 basket:newBasket
-
             }
+
+                case 'SET_USER':
+                    return{
+                        ...state,
+                        user:action.user
+                    }
 
 
         default:
